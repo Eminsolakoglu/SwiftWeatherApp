c4e37ed7155aacf1fec770e323cbe7baeb12a787
Emin Solakoğlu <132267025+Eminsolakoglu@users.noreply.github.com>
2025-07-03 12:05:39 +0300

    Delete SwiftUITestApp/SwiftUITestApp directory

--- SwiftUITestApp/SwiftUITestApp/Utilities/Constants.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-struct Constants{
-    static let openWeatherAPIKey = "5609fc7f013ef18135c82b89e1ad904c"
-    static let openWeatherBaseURL = "https://api.openweathermap.org/data/2.5/"
-}
